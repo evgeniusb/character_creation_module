badf863c424f5f11a12c69e7f95e392983088882
evgeniusb <dos18i2-github@yahoo.com>
2024-06-09 18:26:49 +0300

    Add if __name__ == '__main__':

--- main.py
+++ main.py
@@ -82,7 +82,7 @@ def choice_char_class() -> str:
     return char_class
 
 
-def main():
+if __name__ == '__main__':
     run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
@@ -93,6 +93,3 @@ def main():
     print('Воитель, Маг, Лекарь')
     char_class: str = choice_char_class()
     print(start_training(char_name, char_class))
-
-
-main()
